a06c649747139235f5b1ce7be7b646debed3abd8
adam.html <adam.html@kakaomobility.com>
2018-12-20 14:01:18 +0900

    basics

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-redux-todo
 react, redux, redux-thunk, immer
+
+[https://redux.js.org/basics/example](https://redux.js.org/basics/example)
---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import FilterLink from '../containers/FilterLink';
+import { VisibilityFilters } from '../actions';
+
+const Footer = () => {
+  const { SHOW_ALL, SHOW_ACTIVE, SHOW_COMPLETED } = VisibilityFilters;
+
+  return (
+    <div>
+      <span>Show: </span>
+      <FilterLink filter={SHOW_ALL}>All</FilterLink>
+      <FilterLink filter={SHOW_ACTIVE}>Active</FilterLink>
+      <FilterLink filter={SHOW_COMPLETED}>Completed</FilterLink>
+    </div>
+  );
+};
+
+export default Footer;
